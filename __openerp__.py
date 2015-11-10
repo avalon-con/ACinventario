8762e93c54c2af31db1431f518dcf970b7f25b8c
Francisco Vielma <vielma@cryptolab.net>
2015-11-09 20:11:09 -0430

    Descripción inicial del módulo

--- __openerp__.py
+++ __openerp__.py
@@ -1,34 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "acinventario",
-
+    'name': "Avalon - Inventario",
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+        Inventario y Almacenes""",
     'description': """
-        Long description of module's purpose
+    Descripción Larga
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'author': "Avalon Consultores C.A.",
+    'website': "http://www.avalon.com/inventario",
+    'category': 'Inventory',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'license': 'AGPL-3',
+    'depends': ['base','stock','mail'],
     'data': [
         # 'security/ir.model.access.csv',
         'views.xml',
         'templates.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo.xml',
     ],
